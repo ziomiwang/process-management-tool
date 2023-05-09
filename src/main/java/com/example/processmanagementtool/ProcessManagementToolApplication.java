c3b7163d5808141896fb4ba9e98936cc60efb382
ziomiwang <ziomiwang@gmail.com>
2023-05-09 23:56:31 +0200

    init

---
+++ src/main/java/com/example/processmanagementtool/ProcessManagementToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.processmanagementtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessManagementToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessManagementToolApplication.class, args);
+    }
+
+}
